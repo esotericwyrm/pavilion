f9f4af000039b04d78102b68b61db806101aee87
esotericwyrm <54365505+esotericwyrm@users.noreply.github.com>
2019-08-21 10:31:05 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,23 @@
+/*******************************************************************
+* Hello.java
+*******************************************************************
+* Setup and install Java, run the following line of code 
+* Xavier Thompson
+* 8/21/2019
+* CMSC 255 Section 3
+*******************************************************************/
+
+public class Hello {
+  public static void printHeading() {
+    System.out.println("Your Name");
+	System.out.println("Java Project Name");
+	System.out.println("Project Description");
+	System.out.println("Version Date");
+	System.out.println();
+   }
+   
+  public static void main(String [] args) {
+   printHeading();
+   System.out.println("Hello");
+  }
+ }
